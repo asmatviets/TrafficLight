8b465643747fac9f635a93972452431fa485ae26
AndreyMatviets <asmatviets@gmail.com>
2023-03-19 11:18:54 +0400

    code and logic to turn on - turn off the lights

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var labelButton: UIButton!
+    
+    enum Colors {
+        case red, yellow, green
+    }
+    
+    private var currentColorLight = Colors.red
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeStyle()
     }
 
-
+    @IBAction func buttonAction() {
+        labelButton.setTitle("Next", for: .normal)
+        
+        switch currentColorLight {
+        case .red:
+            turnOnLights()
+            currentColorLight = .yellow
+        case .yellow:
+            turnOnLights()
+            currentColorLight = .green
+        case .green:
+            turnOnLights()
+            currentColorLight = .red
+        }
+    }
+    
+    private func changeStyle() {
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
+        
+        labelButton.layer.cornerRadius = 10
+        
+        currentColorLight = .red
+    }
+    
+    private func turnOnLights() {
+        switch currentColorLight {
+        case .red:
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        case .green:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        }
+    }
 }
 
